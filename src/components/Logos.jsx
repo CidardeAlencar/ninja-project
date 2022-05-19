b4e911f36c90f9827adf3b42839ec8a2b2da8e49
ruddy_flores <ninjaneja@gmail.com>
2022-05-18 20:37:50 -0400

    Listas-Ruddy

--- src/components/Logos.jsx
+++ src/components/Logos.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import LogoNinja from '../assets/LOGO NINJA.png'
 import TextoNinja from '../assets/TEXTO NINJA.png'
 import LogoUsuario from '../assets/LOGO USUARIO.png'
-import  {Menu} from './Menu.jsx'
+// import  {Menu} from './Menu.jsx' 
 import '../styles/Logos.css'
 function Logos() {
   return (
     <div className='Molecula2'>
-          <Menu/>
+          {/* <Menu/> */}
         <img src={LogoNinja} alt='logo ninja'/>
         <img src={TextoNinja} alt='texto ninja'/>
+      
         <img src={LogoUsuario} alt='Logo Usuario' id='usuario'/>
     </div>
   )
