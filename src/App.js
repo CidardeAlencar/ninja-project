89422effd2e8787f492a6a82f3190f17b43e81ab
Kenia Machaca <kenia.my77@gmail.com>
2022-05-19 10:34:13 -0400

    [ad] final fuerzas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import {Header} from './components/Header.jsx';
 import Imagen from './imagen/Imagen.jsx'
+import { Fuerza } from './components/Fuerzas.jsx';
 import { Lomasvisto } from './components/Lomasvisto';
 
 function App() {
   return (
     <><Header/>
-    <Imagen/> <Lomasvisto/> </>
+    <Imagen/> 
+    <Fuerza/>
+    <Lomasvisto/> </>
   );
 }
 
 export default App;
+
